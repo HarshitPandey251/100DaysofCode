5d4f768e8762e93bb0ddd3e901e5248dd1f84aeb
Harshit Pandey <69616901+HarshitPandey251@users.noreply.github.com>
2021-05-20 14:16:45 +0530

    Add files via upload

---
+++ Day2.cpp
@@ -0,0 +1,30 @@
+/*
+** Jump Game II **
+In which Each element in the array represents maximum jump length at that position. So I have solve using Greedy approach
+*/
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        
+        int jumps = 0;
+        int cur_end = 0, cur_max_reach = 0;
+        
+        /*
+        i stops at n-2, 
+        that's because we don't need to jump 
+        if we have reached n-1
+        */
+        for(int i = 0; i < n-1; ++i){
+            cur_max_reach = max(cur_max_reach, i + nums[i]);
+            
+            if(i == cur_end){
+                cur_end = cur_max_reach;
+                ++jumps;
+            }
+        }
+        
+        return jumps;
+    }
+};
